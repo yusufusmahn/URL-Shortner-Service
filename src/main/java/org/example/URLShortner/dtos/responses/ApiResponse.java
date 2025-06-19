913b152928434c4c624a31db5a3d18cc603c652d
yusufusman <yusuf.usman1003@gmail.com>
2025-06-19 20:09:02 +0100

    new task added

---
+++ src/main/java/org/example/URLShortner/dtos/responses/ApiResponse.java
@@ -0,0 +1,12 @@
+package org.example.URLShortner.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private Object data;
+    private boolean success;
+
+}
