e217932fd4360edf5118a4503a41ab1024d2da9d
yusufusman <yusuf.usman1003@gmail.com>
2025-06-23 12:27:31 +0100

    update

---
+++ src/main/java/org/URLShortner/URLShorterServiceApplication.java
@@ -0,0 +1,12 @@
+package org.URLShortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class URLShorterServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(URLShorterServiceApplication.class, args);
+    }
+}
