7b0ba266a38ea52d8c2a7e73a26f1d57e7340d22
yusufusman <yusuf.usman1003@gmail.com>
2025-06-23 14:05:33 +0100

    update

--- src/main/java/org/URLShortner/config/WebConfig.java
+++ src/main/java/org/URLShortner/config/WebConfig.java
@@ -9,30 +9,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // CORS settings for API endpoints
         registry.addMapping("/api/**")
                 .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-
-        // Catch-all fallback rule (optional, for debugging)
-        registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-
-
-        registry.addMapping("/redirect/**")
-                .allowedOrigins("http://127.0.0.1:5500")
-                .allowedMethods("GET", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization", "Location") // Expose Location header
-                .allowCredentials(true);
-
-
     }
+
 
 
 }
