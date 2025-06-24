e9c77cd38647a306628919efca07d0ad80c498cf
yusufusman <yusuf.usman1003@gmail.com>
2025-06-24 17:07:19 +0100

    update

---
+++ src/main/java/org/URLShortner/data/models/User.java
@@ -0,0 +1,14 @@
+package org.URLShortner.data.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+}
